122a2b53b271fe1ca6c0e6c92cd8faebd24e2464
LiamrayWong <7939060+LiamrayWong@user.noreply.gitee.com>
2021-01-19 20:01:30 +0800

    服务端获取数据

--- src/server-entry.js
+++ src/server-entry.js
@@ -1,8 +1,8 @@
 import createApp from './createApp'
 
 export default context => {
-  return new Promise((resolve,reject)=>{
-    const { app, router } = createApp();
+  return new Promise((resolve, reject) => {
+    const { app, router, store } = createApp();
     // 设置服务器端 router 的位置
     router.push(context.url)
     // onReady 等到 router 将可能的异步组件和钩子函数解析完
@@ -10,12 +10,21 @@ export default context => {
       const matchedComponents = router.getMatchedComponents();
       // 匹配不到的路由，执行 reject 函数，并返回 404
       if (!matchedComponents.length) {
-        return reject({
-          code: 404
-        });
+        return reject({ code: 404 })
       }
-      // Promise 应该 resolve 应用程序实例，以便它可以渲染
-      resolve(app)
-    }, reject)
+      // 对所有匹配的路由组件调用 `asyncData()`
+      Promise.all(matchedComponents.map(Component => {
+        if (Component.asyncData) {
+          return Component.asyncData({
+            store,
+            route: router.currentRoute
+          });
+        }
+      })).then(() => {
+        // 状态传递给renderer的上下文，方便后面客户端激活数据
+        context.state = store.state;
+        resolve(app)
+      }).catch(reject);
+    }, reject);
   })
 }
