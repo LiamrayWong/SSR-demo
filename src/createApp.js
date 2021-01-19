6ba27a709bed30314d1f9918ab25e6d6b7567763
LiamrayWong <7939060+LiamrayWong@user.noreply.gitee.com>
2021-01-19 19:55:25 +0800

    store注入到vue实例

--- src/createApp.js
+++ src/createApp.js
@@ -1,15 +1,18 @@
 import Vue from 'vue';
 import App from './App';
 import createRouter from './createRouter';
+import createStore from "@/createStore";
 
-export default function createApp () {
+export default function createApp() {
   const router = createRouter();
+  const store = createStore
   const app = new Vue({
     router,
     render: h => h(App)
   });
   return {
     app,
-    router
+    router,
+    store
   };
 }
