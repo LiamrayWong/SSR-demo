d70793f9ed41bc5e2213adb0fade1cf4aff607bd
LiamrayWong <7939060+LiamrayWong@user.noreply.gitee.com>
2021-01-19 15:34:24 +0800

    创建 html 渲染器

--- src/server.js
+++ src/server.js
@@ -2,7 +2,12 @@
 const path = require('path');
 const serverBundle = path.resolve(process.cwd(), 'serverDist', 'vue-ssr-server-bundle.json');
 const {createBundleRenderer} = require('vue-server-renderer');
-const serverBundle = path.resolve(process.cwd(), 'serverDist', 'vue-ssr-server-bundle.json');
 //加载客户端构建结果
 const clientManifestPath = path.resolve(process.cwd(), 'dist', 'vue-ssr-client-manifest.json');
 const clientManifest = require(clientManifestPath);
+//创建 html 渲染器
+const template = fs.readFileSync(path.resolve(__dirname, 'index.html'), 'utf-8');
+const renderer = createBundleRenderer(serverBundle, {
+  template,  // 使用HTML模板
+  clientManifest // 将客户端的构建结果清单传入
+});
