a90b4e4c775b5ba1602149bd7873da002157550c
LiamrayWong <7939060+LiamrayWong@user.noreply.gitee.com>
2021-01-19 19:57:41 +0800

    模拟远程服务函数fetchItem

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,3 +1,21 @@
 <template>
-  <div>Home</div>
+  <div>
+    <div>id: {{item.id}}</div>
+    <div>name: {{item.name}}</div>
+  </div>
 </template>
+
+<script>
+export default {
+  asyncData({ store, route }) {
+    // 触发 action 后，会返回 Promise
+    return store.dispatch('fetchItems', route.params.id)
+  },
+  computed: {
+    // 从 store 的 state 对象中的获取 item。
+    item() {
+      return this.$store.state.item;
+    }
+  }
+}
+</script>
