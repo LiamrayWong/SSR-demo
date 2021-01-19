3e88c367f1ced2e84d5b1c7e62602952dd7ea6df
LiamrayWong <7939060+LiamrayWong@user.noreply.gitee.com>
2021-01-19 15:31:42 +0800

    加载客户端构建结果

--- src/server.js
+++ src/server.js
@@ -1,4 +1,8 @@
+//加载服务端构建结果
 const path = require('path');
 const serverBundle = path.resolve(process.cwd(), 'serverDist', 'vue-ssr-server-bundle.json');
 const {createBundleRenderer} = require('vue-server-renderer');
 const serverBundle = path.resolve(process.cwd(), 'serverDist', 'vue-ssr-server-bundle.json');
+//加载客户端构建结果
+const clientManifestPath = path.resolve(process.cwd(), 'dist', 'vue-ssr-client-manifest.json');
+const clientManifest = require(clientManifestPath);
